70e556650735aacee722177c9d6e86a9ebbf54eb
Artsiom Kraskouski <artsiom.kraskouski@instinctools.ru>
2019-01-17 11:54:22 +0300

    initial commit

---
+++ src/main/kotlin/com/github/akraskovski/trade/desk/stub/TradeDeskStubApplication.kt
@@ -0,0 +1,12 @@
+package com.github.akraskovski.trade.desk.stub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TradeDeskStubApplication
+
+fun main(args: Array<String>) {
+    runApplication<TradeDeskStubApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/github/akraskovski/trade/desk/stub/TradeDeskStubApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.github.akraskovski.trade.desk.stub
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TradeDeskStubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
