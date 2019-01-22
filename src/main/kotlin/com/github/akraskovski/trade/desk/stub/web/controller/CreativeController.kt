3f106ed2831c02d0a19983b3a9206ad89583c5d2
Artsiom Kraskouski <artsiom.kraskouski@instinctools.ru>
2019-01-22 12:47:28 +0300

    creative search endpoint implementation

--- src/main/kotlin/com/github/akraskovski/trade/desk/stub/web/controller/CreativeController.kt
+++ src/main/kotlin/com/github/akraskovski/trade/desk/stub/web/controller/CreativeController.kt
@@ -2,9 +2,12 @@ package com.github.akraskovski.trade.desk.stub.web.controller
 
 import com.github.akraskovski.trade.desk.stub.web.converter.toResponse
 import com.github.akraskovski.trade.desk.stub.web.form.creative.CreativeForm
+import com.github.akraskovski.trade.desk.stub.web.form.search.CreativeSearchQuery
 import com.github.akraskovski.trade.desk.stub.web.response.creative.CreativeResponse
+import com.github.akraskovski.trade.desk.stub.web.response.search.PageResponse
 import com.github.akraskovski.trade.desk.stub.web.service.CreativeService
 import com.github.akraskovski.trade.desk.stub.web.service.SearchService
+import com.github.akraskovski.trade.desk.stub.web.service.toResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -28,4 +31,11 @@ class CreativeController(
     @PostMapping
     fun create(@RequestBody @Valid creativeForm: CreativeForm): ResponseEntity<CreativeResponse> =
         ResponseEntity.ok(creativeService.create(creativeForm).toResponse())
+
+    /**
+     * Search the campaigns for a given advertiser with predefined criteria.
+     */
+    @PostMapping("/query/advertiser")
+    fun search(@RequestBody @Valid searchQuery: CreativeSearchQuery): ResponseEntity<PageResponse<CreativeResponse>>? =
+        ResponseEntity.ok(creativeService.searchByParent(searchQuery).toResponse())
 }
--- src/main/kotlin/com/github/akraskovski/trade/desk/stub/web/service/CreativeService.kt
+++ src/main/kotlin/com/github/akraskovski/trade/desk/stub/web/service/CreativeService.kt
@@ -3,14 +3,20 @@ package com.github.akraskovski.trade.desk.stub.web.service
 import com.github.akraskovski.trade.desk.stub.domain.model.creative.ImageCreative
 import com.github.akraskovski.trade.desk.stub.domain.repository.ImageCreativeRepository
 import com.github.akraskovski.trade.desk.stub.web.converter.toImageCreative
+import com.github.akraskovski.trade.desk.stub.web.converter.toResponse
 import com.github.akraskovski.trade.desk.stub.web.form.creative.CreativeForm
+import com.github.akraskovski.trade.desk.stub.web.form.search.CreativeSearchQuery
+import com.github.akraskovski.trade.desk.stub.web.response.creative.CreativeResponse
 import org.springframework.stereotype.Service
 
 /**
  * Basic creative service.
  */
 @Service
-class CreativeService(private val imageCreativeRepository: ImageCreativeRepository) {
+class CreativeService(
+    private val imageCreativeRepository: ImageCreativeRepository,
+    private val searchService: SearchService
+) {
 
     /**
      * Create and store in db creative entity.
@@ -19,5 +25,16 @@ class CreativeService(private val imageCreativeRepository: ImageCreativeReposito
         creativeForm.imageAttributes ?: throw UnsupportedOperationException("Image attributes should be not empty")
 
         return imageCreativeRepository.save(creativeForm.toImageCreative())
+    }
+
+    /**
+     * Search all existing creative aggregated in creative response.
+     */
+    fun searchByParent(query: CreativeSearchQuery): List<CreativeResponse> {
+        val result = mutableListOf<CreativeResponse>()
+        result.addAll(searchService.searchByParent(query, imageCreativeRepository).map(ImageCreative::toResponse))
+        //here could be any other creative repositories search
+
+        return result
     }
 }
